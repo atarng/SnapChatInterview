aa88aa45fe401ee7d729d0b34cb9ea614e27a208
Alfred Tarng <atarng.A5T@gmail.com>
2017-06-06 17:01:23 -0700

    OpenCV C++ Native Code

--- OpenCVTest/Test.cpp
+++ OpenCVTest/Test.cpp
@@ -1,5 +1,3 @@
-//#include "Test.h"
-
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -10,78 +8,104 @@
 using namespace std;
 using namespace cv;
 
+int g_cameraWidth, g_cameraHeight;
+CascadeClassifier _faceCascade;
+String _windowName = "UnityOpenCV (Test)";
+
 struct Circle
 {
     Circle(int x, int y, int radius) : X(x), Y(y), Radius(radius) {}
     int X, Y, Radius;
 };
 
-CascadeClassifier _faceCascade;
-String _windowName = "UnityOpenCV (Test)";
-VideoCapture _capture;
-int _scale = 1;
+extern "C" void __declspec(dllexport) __stdcall  Init(int cameraWidth, int cameraHeight)
+{
+    g_cameraWidth = cameraWidth;  //_capture.get(CAP_PROP_FRAME_WIDTH);
+    g_cameraHeight = cameraHeight; //_capture.get(CAP_PROP_FRAME_HEIGHT);
+}
 
-extern "C" int __declspec(dllexport) __stdcall  Init(int& outCameraWidth, int& outCameraHeight)
+extern "C" __declspec(dllexport) int __stdcall  LoadCascade(const char* str)
 {
     // Load LBP face cascade.
-    if (!_faceCascade.load("lbpcascade_frontalface.xml"))
+    if (!_faceCascade.load(str))//  "Cascades/lbpcascade_frontalface.xml"))
         return -1;
-
-    // Open the stream.
-    _capture.open(0);
-    if (!_capture.isOpened())
-        return -2;
-
-    outCameraWidth = _capture.get(CAP_PROP_FRAME_WIDTH);
-    outCameraHeight = _capture.get(CAP_PROP_FRAME_HEIGHT);
 
     return 0;
 }
 
-extern "C" void __declspec(dllexport) __stdcall  Close()
+//__stdcall 
+extern "C" __declspec(dllexport) char* ocv_get_image(char* pixels_as_char)
 {
-    _capture.release();
+    Mat src_frame(g_cameraHeight, g_cameraWidth, CV_8UC4, pixels_as_char);
+    Mat cv__frame(g_cameraHeight, g_cameraWidth, CV_8UC3, Scalar(0, 0, 0));
+
+    cvtColor(src_frame, cv__frame, CV_RGBA2BGR, 4);
+    threshold(cv__frame, cv__frame, 128, 128, THRESH_BINARY);
+    cvtColor(cv__frame, cv__frame, CV_BGR2RGBA, 4);
+
+    char* result;
+    result = new char[cv__frame.cols*cv__frame.rows * 4];
+    memcpy(result, cv__frame.data, cv__frame.cols*cv__frame.rows * 4);
+
+    return result;
 }
 
-extern "C" void __declspec(dllexport) __stdcall SetScale(int scale)
-{
-    _scale = scale;
-}
 
-extern "C" void __declspec(dllexport) __stdcall Detect(Circle* outFaces, int maxOutFacesCount, int& outDetectedFacesCount)
-{
-    Mat frame;
-    _capture >> frame;
-    if (frame.empty())
-        return;
+extern "C"  __declspec(dllexport) char* __stdcall DetectAndDrawFace(
+    char* pixels_as_char, Circle* outFaces, int maxOutFacesCount, int& outDetectedFacesCount
+){
+    Mat src_frame(g_cameraHeight, g_cameraWidth, CV_8UC4, pixels_as_char);
+    Mat cv__frame(g_cameraHeight, g_cameraWidth, CV_8UC3, Scalar(0, 0, 0));
+
+    // src -> cv
+    cvtColor(src_frame, cv__frame, CV_RGBA2BGR, 4);
+
+
+    // invert Height
+    flip(cv__frame, cv__frame, 0);
 
     std::vector<Rect> faces;
     // Convert the frame to grayscale for cascade detection.
+    // cv --> grayscale
     Mat grayscaleFrame;
-    cvtColor(frame, grayscaleFrame, COLOR_BGR2GRAY);
-    Mat resizedGray;
+    cvtColor(cv__frame, grayscaleFrame, COLOR_BGR2GRAY);
+
     // Scale down for better performance.
-    resize(grayscaleFrame, resizedGray, Size(frame.cols / _scale, frame.rows / _scale));
-    equalizeHist(resizedGray, resizedGray);
+    //Mat resizedGray;
+    //resize(grayscaleFrame, resizedGray, Size(frame.cols / _scale, frame.rows / _scale));
+    //equalizeHist(resizedGray, resizedGray);
 
     // Detect faces.
-    _faceCascade.detectMultiScale(resizedGray, faces);
+    _faceCascade.detectMultiScale(grayscaleFrame, faces);
 
     // Draw faces.
     for (size_t i = 0; i < faces.size(); i++)
     {
-        Point center(_scale * (faces[i].x + faces[i].width / 2), _scale * (faces[i].y + faces[i].height / 2));
-        ellipse(frame, center, Size(_scale * faces[i].width / 2, _scale * faces[i].height / 2), 0, 0, 360, Scalar(0, 0, 255), 4, 8, 0);
+        //_scale *
+        Point center((faces[i].x + faces[i].width / 2), (faces[i].y + faces[i].height / 2));
+        ellipse(cv__frame, center, //cv__frame, center,
+            Size(faces[i].width / 2, faces[i].height / 2),
+            0, 0, 360,
+            Scalar(0, 0, 255),
+            4, 8, 0);
 
         // Send to application.
         outFaces[i] = Circle(faces[i].x, faces[i].y, faces[i].width / 2);
         outDetectedFacesCount++;
-
         if (outDetectedFacesCount == maxOutFacesCount)
             break;
     }
 
-    // Display debug output.
-    // probably don't want to have this on the mobile version.
-    imshow(_windowName, frame);
+    //imshow(_windowName, cv__frame);
+
+    // reverseFlip
+    flip(cv__frame, cv__frame, 0);
+    // convert back to RGBA
+    cvtColor(cv__frame, cv__frame, CV_BGR2RGBA, 4);
+
+    char* result;
+    result = new char[cv__frame.cols*cv__frame.rows * 4];
+    memcpy(result, cv__frame.data, cv__frame.cols*cv__frame.rows * 4);
+
+    return result;
 }
